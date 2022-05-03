6d057b5789c31f354c143c318807cdcb156fcb83
Bastian Jung <bastian.jung@rwth-aachen.de>
2022-05-03 11:33:35 +0200

    computer modern

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
     setuptools.setup(
             name="BeautifulPlots",
-            version="0.1.0",
+            version="0.2.0",
             author="Bastian Jung",
             author_email="jungb@student.chalmers.se",
             description="Making matplotlib look a little nicer",
