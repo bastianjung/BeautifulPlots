326cf1175348942f32d382f5508b63d2fa92a034
Bastian Jung <bastian.jung@rwth-aachen.de>
2022-05-03 11:38:00 +0200

    revert

--- BeautifulPlots/reset_style.py
+++ BeautifulPlots/reset_style.py
@@ -6,8 +6,3 @@ def reset_style():
     plt.style.use(pkg_resources.resource_filename('BeautifulPlots.styles', 'style_white_serif.mplstyle'))
     matplotlib.rcParams['pdf.fonttype'] = 42
     matplotlib.rcParams['ps.fonttype'] = 42
-    matplotlib.rcParams.update({
-        "text.usetex": True,
-        "font.family": "serif",
-        "font.serif": ["Computer Modern Roman"],
-    })
